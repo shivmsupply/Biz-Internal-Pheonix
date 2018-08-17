f59b9cf97fe65758a9d5b01e5c46d75177b10f38
vishnu-msupply <vishnu@msupply.com>
2018-08-17 17:11:29 +0530

    left panel

--- src/index.js
+++ src/index.js
@@ -3,8 +3,13 @@ import ReactDOM from 'react-dom';
 import { App } from './App';
 // import 'bootstrap/dist/css/bootstrap.min.css';
 // import 'bootstrap';
+
 // import './assets/styles/style.css';
 // import './assets/styles/app.scss';
+
+// import './assets/styles/style.css';
+import './assets/styles/common.css';
+
 
 ReactDOM.render(<App />, document.getElementById('app'));
 
